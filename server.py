59d3768dd2985eed34373cd540fe3944f758ef51
Aimen <aimenhamed@live.com.au>
2021-07-09 13:42:40 +1000

    filled up new line bytes

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
-RECEIVED = '\n' * 30
+RECEIVED = '\n' * 32
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(ADDR)
